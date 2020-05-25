f85de15aac6d6e189c4da0323bed8f779c64be7a
Tijana <tijana.ilic87@protonmail.com>
2020-05-25 12:20:25 +0200

    Create Questions API controller

---
+++ ReactQuiz/Controllers/QuestionsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReactQuiz.Data;
+
+namespace ReactQuiz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuestionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetQuestions()
+        {
+            var questions = await _context.Questions.Include(q => q.Answers).ToListAsync();
+
+            if (questions == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(questions);
+        }
+
+    }
+}
