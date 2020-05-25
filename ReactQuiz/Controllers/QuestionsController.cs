3770410090bb34ef5284e5ad1db9cf09ebdf07f0
Andreea Nenciu <andreeanen@yahoo.com>
2020-05-25 14:28:23 +0200

    Change quizData into state from var

--- ReactQuiz/Controllers/QuestionsController.cs
+++ ReactQuiz/Controllers/QuestionsController.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ReactQuiz.Data;
-using ReactQuiz.Models;
+using System.Threading.Tasks;
 
 namespace ReactQuiz.Controllers
 {
@@ -23,7 +17,7 @@ namespace ReactQuiz.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Question>>> GetQuestions()
+        public async Task<IActionResult> GetQuestions()
         {
             var questions = await _context.Questions.Include(q => q.Answers).ToListAsync();
 
