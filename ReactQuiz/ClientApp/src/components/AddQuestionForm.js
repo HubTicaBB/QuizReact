40ee2fdf629c2356986ba56041dd75eabbee185f
Andreea Nenciu <andreeanen@yahoo.com>
2020-06-01 12:11:15 +0200

    Add form component to add question

---
+++ ReactQuiz/ClientApp/src/components/AddQuestionForm.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+
+export class AddQuestionForm extends React.Component {
+
+    render() {
+        return (
+            <div>
+
+                <p className="mb-3">Here comes the form</p>
+
+            </div >
+        )
+    }
+}
--- ReactQuiz/ClientApp/src/components/Admin.js
+++ ReactQuiz/ClientApp/src/components/Admin.js
@@ -1,14 +1,21 @@
 ﻿import React from 'react';
 import authService from './api-authorization/AuthorizeService';
 import { Table, Card, CardTitle, ListGroup, ListGroupItem, CardFooter, CardText, ButtonGroup, Button } from 'reactstrap';
+import { AddQuestionForm } from './AddQuestionForm';
+
 
 export class Admin extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             questions: [],
-            isLoaded: false
+            isLoaded: false,
+            isAddQuestionButtonClicked: false
         };
+    }
+
+    handleAddQuestion = () => {
+        this.setState({ isAddQuestionButtonClicked: true });
     }
 
     async componentDidMount() {
@@ -24,33 +31,45 @@ export class Admin extends React.Component {
     render() {
         return (
             <div>
-                <div> <Button size="lg" className=" mb-4 mt-4">Add Question</Button>
+                <div> <Button size="lg" className=" mb-4 mt-4" onClick={this.handleAddQuestion}>Add Question</Button>
                 </div>
-                <div>
 
-                    {this.state.questions.map((question) =>
-                        <Card className="mt-2 mb-4 " key={question.id}>
-                            <CardTitle className="m-2 h3">Question {question.id}: {question.content}</CardTitle>
 
-                            <ListGroup className="m-2 "> Answers:  {
-                                question.answers.map((answer) =>
-                                    <ListGroupItem className="m-2" key={answer.id} >
-                                        {answer.content}
-                                    </ListGroupItem>
-                                )}
-                            </ListGroup>
-                            <CardText className="m-2">Correct answer:{question.correctAnswer}</CardText>
-                            <CardFooter className="bg-white">
-                                <ButtonGroup size="lg" className="mb-2 float-right">
-                                    <Button>Edit</Button>
-                                    <Button>Delete</Button>
+                {(this.state.isAddQuestionButtonClicked) ?
+                    (
+                        <AddQuestionForm />
 
-                                </ButtonGroup>
-                            </CardFooter>
-                        </Card>
-                    )}
+                    ) :
+                    (
+                        <div>
 
-                </div>
+                            {this.state.questions.map((question) =>
+                                <Card className="mt-2 mb-4 " key={question.id}>
+                                    <CardTitle className="m-2 h3">Question {question.id}: {question.content}</CardTitle>
+
+                                    <ListGroup className="m-2 "> Answers:  {
+                                        question.answers.map((answer) =>
+                                            <ListGroupItem className="m-2" key={answer.id} >
+                                                {answer.content}
+                                            </ListGroupItem>
+                                        )}
+                                    </ListGroup>
+                                    <CardText className="m-2">Correct answer:{question.correctAnswer}</CardText>
+                                    <CardFooter className="bg-white">
+                                        <ButtonGroup size="lg" className="mb-2 float-right">
+                                            <Button>Edit</Button>
+                                            <Button>Delete</Button>
+
+                                        </ButtonGroup>
+                                    </CardFooter>
+                                </Card>
+                            )}
+
+                        </div>
+                    )
+                }
+
+
             </div>
 
         );
